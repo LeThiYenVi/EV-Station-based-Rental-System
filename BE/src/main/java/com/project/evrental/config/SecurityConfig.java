7f5b143a1dec7638aaee16ff4c197adb0c303f60
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-12-09 16:00:50 +0700

    okoko

--- BE/src/main/java/com/project/evrental/config/SecurityConfig.java
+++ BE/src/main/java/com/project/evrental/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package com.project.evrental.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins; // Inject biến vào đây
 
     public String[] public_endpoints = {
             "/api/auth/**",
@@ -18,7 +27,8 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/api/stations/**",
             "/api/vehicles/**",
-            "/api/payments/momo/callback"
+            "/api/payments/momo/callback",
+            "/actuator/*"
     };
 
     @Bean
@@ -35,7 +45,28 @@ public class SecurityConfig {
                                         .jwtAuthenticationConverter(new JwtAuthConverter())
                         )
                 );
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        if (allowedOrigins != null) {
+            Arrays.stream(allowedOrigins.split(","))
+                    .map(String::trim)
+                    .forEach(configuration::addAllowedOrigin);
+        }
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Áp dụng cho mọi API
+        return source;
     }
 }
