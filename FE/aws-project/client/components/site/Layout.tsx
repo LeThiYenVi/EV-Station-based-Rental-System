f4dc997291dec29846d8ce76f037f23c29888ab9
TruongNguyenThaiBinh77 <binhtntse182370@fpt.edu.vn>
2025-12-11 00:26:25 +0700

    uoc gi tao bot dang cap mut chutttt

--- FE/aws-project/client/components/site/Layout.tsx
+++ FE/aws-project/client/components/site/Layout.tsx
@@ -3,10 +3,34 @@ import { Footer } from "./Footer";
 import { ReactNode } from "react";
 import "./layout.css";
 import ChatBot from "@/components/ChatBot";
+import XmasTree from "react-xmas-tree/react";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="layout-container">
+      {/* Christmas Tree with react-xmas-tree */}
+      <XmasTree
+        position={{
+          left: "30px",
+          bottom: "10px",
+          position: "fixed",
+        }}
+        starColor="#60a5fa"
+        lightColors={[
+          "#0EA5E9", // Sky blue
+          "#06B6D4", // Cyan
+          "#3B82F6", // Blue
+          "#6366F1", // Indigo
+          "#8B5CF6", // Purple
+          "#10b981", // Emerald green
+        ]}
+        customLightStyles={{
+          width: "1.5vmin",
+          height: "1.5vmin",
+          borderRadius: "50%",
+        }}
+      />
+
       <Header />
       <main className="layout-main">{children}</main>
       <Footer />
